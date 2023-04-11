aea367d8241278849453cbcfa505038776c1c57c
jannat <jma24934@gmail.com>
2023-04-11 13:01:29 -0700

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+has class basegeometry
+'''
+
+
+class BaseGeometry:
+    ''' class with public attribute area'''
+
+    def area(self):
+        ''' raises an exceptio when called'''
+
+        raise Exception("area() is not implemented")
