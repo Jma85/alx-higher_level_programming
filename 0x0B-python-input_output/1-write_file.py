98e9ba2f34a890ae21d6c14195b820c77e7ffb48
jannat <jma24934@gmail.com>
2023-04-16 09:01:20 -0700

    writefile

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,6 @@ contains the function write_file()
 
 
 def write_file(filename="", text=""):
-    with open(filename, 'w', encoding='utf=8') as a_file:
-        return a_file.write(text)
+    ''' a function that writes a string to a text'''
+    with open(filename, 'w', encoding='utf=8') as f:
+        return f.write(text)
